4a7d823f6baf0b1cad969f5ab5d7cae1fda3584e
Prayush <prayushkale@gmail.com>
2020-08-15 16:29:59 +0530

    First commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import "tailwindcss/dist/base.min.css";
+import "tailwindcss/dist/components.min.css";
+import "tailwindcss/dist/utilities.min.css";
+
 if (environment.production) {
   enableProdMode();
 }
